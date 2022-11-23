8d8d6c84d5cd95a5037b17a1ef47a7dc89aa2df0
Hannah <h.dunn@spin.net.au>
2022-11-23 12:01:44 +1100

    Added in a basic date and time to top of project

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+// const dt = new Date();
+// document.getElementById('date-time').innerHTML=dt;
+
+
+function display_c(){
+    let refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct()',refresh)
+ }
+ 
+ function display_ct() {
+   let CDate = new Date()
+   let NewDate=CDate.toDateString(); 
+   NewDate = NewDate + " - " + CDate.toLocaleTimeString();
+   document.getElementById('ct').innerHTML = NewDate;
+   display_c();
+ }
+
+
+
+
+
+
+
+
 function validateTaskForm() {
     let x = document.forms["exampleModalCenter"]["inputName3"].value;
     if (x == "") {
@@ -7,12 +32,12 @@ function validateTaskForm() {
   }
 
 
-Create a JavaScript function called “validateTaskForm” that verifies 
-that the inputs inserted by the user in the task form are correct:
-Name -> Not Empty and longer than 8 characters
-Description -> Not Empty and longer than 15 characters
-AssignedTo -> Not Empty and longer than 8 characters
-DueDate -> Not Empty and not less than current date
+// Create a JavaScript function called “validateTaskForm” that verifies 
+// that the inputs inserted by the user in the task form are correct:
+// Name -> Not Empty and longer than 8 characters
+// Description -> Not Empty and longer than 15 characters
+// AssignedTo -> Not Empty and longer than 8 characters
+// DueDate -> Not Empty and not less than current date
 
 
 
