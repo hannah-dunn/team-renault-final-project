903265a1ef7ff6ed40566045a59fbcf1ab105f8b
Hannah <h.dunn@spin.net.au>
2022-11-28 00:18:43 +1100

    it console logs the API

--- script.js
+++ script.js
@@ -73,8 +73,9 @@ function loadPage(){
     fetch("https://jwd09-task-api.herokuapp.com")
     .then(resp => resp.json())
     .then(data => {
-        data.map(eachObject => {
-            
+        data.map(eachObj => {
+            let newTask = new TaskManager(eachObj.id, eachObj.title, eachObj.description, eachObj.assigned_to, eachObj.date, eachObj.setStatus)
+            console.log(newTask)
         })
     })
     .catch(err => console.log(err));
@@ -84,39 +85,14 @@ function loadPage(){
 
 
 
-
-
- let taskForm= document.getElementById('taskForm')
-  let tasksList=[]
-
-
-  window.addEventListener('load',()=>{
-     tasksList=  JSON.parse( localStorage.getItem('Tasks'))
-     console.log(`tasklist :${tasksList}`)
-  })
-  taskForm.addEventListener('submit',(event)=>{
-        event.preventDefault();
-             console.log(taskForm['taskName'].value)
-        const formData=new FormData(taskForm);
-        console.log(`formdata:${formData}`)
-           
-
-           console.log(`task name :${task._taskName}`)
-           console.log(task)
-           tasksList.push(task)
-
-           window.localStorage.setItem('Tasks',JSON.stringify(tasksList))
-  })
-    
-
   class TaskManager{
     static id = 0
-    constructor(id,taskName,description,assignedTo,dueDate,setStatus){
+    constructor(id,title,description,assigned_to,date,setStatus){
       this.id = TaskManager.id++;
-       this.taskName = taskName;
+       this.title = title;
        this.description = description;
-       this.assignedTo = assignedTo;
-       this.dueDate = dueDate;
+       this.assigned_to = assigned_to;
+       this.date = date;
        this.setStatus = setStatus;
       const saveToLocal = () => {
         localStorage.setItem(this._id, this)
@@ -126,109 +102,75 @@ function loadPage(){
       localStorage.setItem(Object._id, Object)
     }
 
-      get id(){
-        return this.id
-      }
-       get taskName(){
-        return this.taskName;
-       }
-       get description(){
-        return this.description;
-       }
-       get assignedTo(){
-        return this.assignedTo;
-       }
-        get dueDate(){
-          return this.dueDate;
-        }
-        get setStatus(){
-          return this.setStatus;
-        }
-      
-      set taskName(newTaskName){
-        this.taskName=newTaskName
-     }
-     set description(newDescription){
-         this.description=newDescription
-     }
-     set assignedTo(newAssignedTo){
-         this.assignedTo=newAssignedTo
-     }
-     set dueDate(newDueDate){
-         this.dueDate=newDueDate
-     }
-     set setStatus(newSetStatus){
-         this.setStatus=newSetStatus
-     }
-    }
-     class TaskManagers{
-         // Each task object should be added to and stored in an array variable
-         constructor(){
-             this.taskMangers=[];
-         }
-     // Add Task -> a task to existing Tasks List
-     addTask(taskName,taskDes,assignTo,dueDate,taskSt){
-         let task=new TaskManger(taskName,taskDes,assignTo,dueDate,taskSt);
-     this.taskMangers.push(task);
-     return task;
-     }
-     // Get Tasks -> returns the list of ALL tasks
-     getAllTasks(){
-         return this.taskMangers
-     }
-     // Get all Tasks with a given status -> returns a list of all tasks where a status equal to the status passes as an argument
-     getTasksWithStatus(status){
-         let filterTask=this.taskMangers.filter(task=>task.status===status)
-         return filterTask
-     }
-     }
-     //Add a card once created with all the details of task.
-     btnSubmit.addEventListener('click',(e)=>{
-     //Form fields data validation
-        if(validate()){
-         //call render method to add new tasks
-          render();
-        }
-     e.preventDefault();
-         })
+  }
 
-//Each time a new task is added, the render() method is called to display the new task.
-const render=()=>{
-  //  add task info into TaskManagers array list 
-  let task= new TaskManagers();
-  task.addTask(taskName.value,taskDes.value,assignTo.value,dueDate.value,taskSt.value,commentIn.value)
- //show task box in HTML
- createTaskHTML(task.getAllTasks());
-  //    reset form fields
-  document.getElementById("form").reset();
-}
-// creates a Card Layout HTML as defined on previous tasks object
-const createTaskHTML=(task)=>{
-task.forEach(element => {
-const taskHTML=` <div class="col">
-     <div class="card mb-3 bg-info m-2 task-box p-1 rounded shadow box-1 text-light" style="max-width: 18rem;">
-       <div class="card-header bg-transparent border-light"><small class="fst-italic" >
-       Due Date:${element.dueDate}</small></div>
-       <div class="card-body text-light">
-         <h5>Name: ${element.taskName}</h5>
-         <p>Description: ${element.description}</p>
-         <p>Assign To:    <select class="form-control col me-3" id="assignedTo" required="required">
-         <option>${element.assignedTo}</option>
-        
-     </select></p>
-         <div class="spinner-border text-light" role="status"> </div>
-             <span >${element.setStatus}</span>
-           
-       <div class="card-footer bg-transparent border-light">              
-           <button type="submit" class="btn btn-success shadow">Update</button>
-           <button type="submit" class="btn btn-danger shadow">Delete</button>
-       </div>
-     </div>
-   </div>`
- const itemsContainer = document.getElementById("taskCards");
- itemsContainer.innerHTML += taskHTML; 
-});     
-}
+
+
+    //   get id(){
+    //     return this.id
+    //   }
+    //    get taskName(){
+    //     return this.taskName;
+    //    }
+    //    get description(){
+    //     return this.description;
+    //    }
+    //    get assignedTo(){
+    //     return this.assignedTo;
+    //    }
+    //     get dueDate(){
+    //       return this.dueDate;
+    //     }
+    //     get setStatus(){
+    //       return this.setStatus;
+    //     }
+      
+    //   set taskName(newTaskName){
+    //     this.taskName=newTaskName
+    //  }
+    //  set description(newDescription){
+    //      this.description=newDescription
+    //  }
+    //  set assignedTo(newAssignedTo){
+    //      this.assignedTo=newAssignedTo
+    //  }
+    //  set dueDate(newDueDate){
+    //      this.dueDate=newDueDate
+    //  }
+    //  set setStatus(newSetStatus){
+    //      this.setStatus=newSetStatus
+    //  }
+    // }
+    //  class TaskManagers{
+    //      // Each task object should be added to and stored in an array variable
+    //      constructor(){
+    //          this.taskMangers=[];
+    //      }
+    //  // Add Task -> a task to existing Tasks List
+    //  addTask(taskName,taskDes,assignTo,dueDate,taskSt){
+    //      let task=new TaskManger(taskName,taskDes,assignTo,dueDate,taskSt);
+    //  this.taskMangers.push(task);
+    //  return task;
+    //  }
+    //  // Get Tasks -> returns the list of ALL tasks
+    //  getAllTasks(){
+    //      return this.taskMangers
+    //  }
+    //  // Get all Tasks with a given status -> returns a list of all tasks where a status equal to the status passes as an argument
+    //  getTasksWithStatus(status){
+    //      let filterTask=this.taskMangers.filter(task=>task.status===status)
+    //      return filterTask
+    //  }
+    //  }
+    //  //Add a card once created with all the details of task.
+    //  btnSubmit.addEventListener('click',(e)=>{
+    //  //Form fields data validation
+    //     if(validate()){
+    //      //call render method to add new tasks
+    //       render();
+    //     }
+    //  e.preventDefault();
+    //      })
 
 
 // to add cards for task 7 maybe
